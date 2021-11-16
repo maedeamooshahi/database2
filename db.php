6458d74c5a8aec39a487bfe3b3eee1496c2737b9
maedeamooshahi <94393666+maedeamooshahi@users.noreply.github.com>
2021-11-16 09:46:31 -0800

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,12 @@
 <?php
     $user = "root";
-	$pass = "";
     $pass = "";
 
-	try{
-		$databaseconnect = new PDO( "mysql:host=localhost;dbname=adb_ex1" , $user , $pass );
-		$databaseconnect->exec("set names utf8");
-	}
-	catch(PDOException $e){
-		header("Location: khata.php");
-	}
     try{
-	$databaseconnect = new PDO( "mysql:host=localhost;dbname=adb_ex2" , $user , $pass );
+	$databaseconnect = new PDO( "mysql:host=localhost;dbname=phpmyadmin" , $user , $pass );
 	$databaseconnect->exec("set names utf8");
     }
     catch(PDOException $e){
-	header("Location: khta.php");
+	header("Location: khata.php");
     }
 ?>
